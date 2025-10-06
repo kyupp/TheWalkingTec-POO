566f8be4ddfc6ac803a959eb86b1623c2f1297c0
mathivl06 <mathiasviquez1006@gmail.com>
2025-10-05 20:32:27 -0600

    avance 1

---
+++ src/main/java/com/mycompany/thewalkingtec/poo/Tropa.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.thewalkingtec.poo;
+
+/**
+ *
+ * @author mathiasviquez
+ */
+public class Tropa extends Componente {
+    
+    int ataque;
+    //boolean isAttacking = true;
+    
+    public int atacar(){
+        return ataque;
+    }
+    
+}
