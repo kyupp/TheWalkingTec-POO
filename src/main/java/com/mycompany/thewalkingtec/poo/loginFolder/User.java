7e92f644e6fa000643f8d0a0e79095c796598c5d
mathivl06 <mathiasviquez1006@gmail.com>
2025-10-23 19:11:28 -0600

    AvancePantallasPt1
    Realización de la pantalla login y primer avance de la aplicación de creación de armas

---
+++ src/main/java/com/mycompany/thewalkingtec/poo/loginFolder/User.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.thewalkingtec.poo.loginFolder;
+
+/**
+ *
+ * @author mathiasviquez
+ */
+public class User {
+    
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+}
